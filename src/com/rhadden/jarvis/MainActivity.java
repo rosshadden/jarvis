23d847e06fce5fee8381c4b4df1b15d68587ae1d
Ross Hadden <rosshadden@gmail.com>
2013-08-10 23:46:55 -0400

    Added text and button, and made the button change the text on press.

--- src/com/rhadden/jarvis/MainActivity.java
+++ src/com/rhadden/jarvis/MainActivity.java
@@ -2,11 +2,22 @@ package com.rhadden.jarvis;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		final TextView welcome = (TextView) findViewById(R.id.welcome);
+		final Button speak = (Button) findViewById(R.id.speak);
+
+		speak.setOnClickListener(new TextView.OnClickListener() {
+			public void onClick(View v) {
+				welcome.setText("Mwahahaha.");
+			}
+		});
 	}
 }
